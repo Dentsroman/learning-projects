396f081ced9ca9d3b7a49d3c4a240e0f511540c8
William Bevia <williambevia@gmail.com>
2024-09-18 23:14:18 +0200

    [2.1.1] Progress on the renderer

--- C++/GameOfLife/src/rendering/Renderer.h
+++ C++/GameOfLife/src/rendering/Renderer.h
@@ -10,20 +10,20 @@
 
 class Renderer {
 public:
-    static Renderer& getInstance(int width = 800, int height = 600, const std::string& title = "2D Grid");
+    static Renderer& getInstance(int32_t width = 800, int32_t height = 600, const std::string& title = "2D Grid");
     void initialize();
     void generateGrid(int numCells, float gridSize);
     void render();
     void run();
 
 private:
-    Renderer(int width, int height, const std::string& title);
+    Renderer(int32_t width, int32_t height, const std::string& title);
     ~Renderer();
     Renderer(const Renderer&) = delete;
     Renderer& operator=(const Renderer&) = delete;
 
-    int width;
-    int height;
+    int32_t width;
+    int32_t height;
     std::string title;
     GLFWwindow* window;
     GLuint shaderProgram;
@@ -33,7 +33,7 @@ private:
     GLuint compileShader(GLenum type, const char* source);
     GLuint createShaderProgram();
     void setupGrid();
-    static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+    static void framebuffer_size_callback(GLFWwindow* window, int32_t width, int32_t height);
 };
 
 #endif // RENDERER_H
